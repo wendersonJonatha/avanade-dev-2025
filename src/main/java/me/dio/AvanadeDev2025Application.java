6d6942a9535a35d61801527bcba6571a39cb7531
wendersonJ <wendersonjonathadev@gmail.com>
2025-03-19 18:03:30 -0300

    Initial commit

---
+++ src/main/java/me/dio/AvanadeDev2025Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanadeDev2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanadeDev2025Application.class, args);
+	}
+
+}
